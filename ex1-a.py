b72e707ada765b80d2791d06be55594be00f928c
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-11-11 23:00:57 +0100

    Exo1

---
+++ ex1-a.py
@@ -0,0 +1,11 @@
+nom_entreprise = "Enedis"
+nombre_de_clients = 1000
+revenu_moyen_par_client = 50000.0
+
+revenu_total = nombre_de_clients * revenu_moyen_par_client
+
+print("Nom de l'entreprise :", nom_entreprise)
+print("Nombre de clients :", nombre_de_clients)
+print("Revenu moyen par client :", revenu_moyen_par_client)
+print("Revenu total :", revenu_total)
+
