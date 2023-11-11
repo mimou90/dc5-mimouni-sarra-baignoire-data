b0f59c1e648a0c5e50432175966cea67648221b0
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-11-11 23:51:27 +0100

    modif gestion erreur

--- ex2-a.py
+++ ex2-a.py
@@ -2,9 +2,12 @@
 
 depenses_mensuelles = [1200, 1500, 800, 1100, 1300, 1000, 900, 1200, 950, 1100, 1300, 1000]
 
-depenses_annuelles = 0
-for depense in depenses_mensuelles:
-    depenses_annuelles += depense
+if not depenses_mensuelles:
+    print("Erreur : la liste des dépenses est vide.")
+else:
+    depenses_annuelles = 0
+    for depense in depenses_mensuelles:
+        depenses_annuelles += depense
 
-print(f"Les dépenses marketing mensuelles sont : {depenses_mensuelles}")
-print(f"Les dépenses totales de l'année sont : {depenses_annuelles}")
+    print(f"Les dépenses marketing mensuelles sont : {depenses_mensuelles}")
+    print(f"Les dépenses totales de l'année sont : {depenses_annuelles}")
