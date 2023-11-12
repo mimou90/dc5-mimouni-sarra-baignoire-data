c8026b48ad5f2872fb3a03c7f81cceb8fe962759
sarra Mimouni <sarra.mimouni@hotmail.fr>
2023-11-12 10:37:33 +0100

    Modif

--- ex4-a.py
+++ ex4-a.py
@@ -9,7 +9,6 @@ def calculer_taux_clics_pourcentage(clics, impressions):
     return taux_clics
 
 # Tests
-
 print(f"Taux de clics : {calculer_taux_clics_pourcentage(150, 1000):.2f}%")
 print(f"Taux de clics : {calculer_taux_clics_pourcentage(300, 10000):.2f}%")
 print(f"Taux de clics : {calculer_taux_clics_pourcentage(75, 500):.2f}%")
